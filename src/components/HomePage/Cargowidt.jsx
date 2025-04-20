a08fd85fc6f28c93a6b8d5859fb1c1ef577516b3
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2025-04-21 00:18:53 +0700

    style: adjust warehouse div style

--- src/components/HomePage/Cargowidt.jsx
+++ src/components/HomePage/Cargowidt.jsx
@@ -16,12 +16,12 @@ export default function Cargowidt({ isMobile }) {
                 : <div className="w-full overflow-hidden mt-6">
                     <h1 className='font-bold text-2xl text-center'>CargoWIDT</h1>
                     <div className='text-sm font-light underline text-blue-800 mt-3 text-center'> <CopyText text={"https://cargowidt-client.onrender.com"} /></div>
-                    <div className='flex items-center justify-center gap-4 mx-4 mt-4'>
-                        <div>
-                            <img src={cargoWIDT} alt="cargoWIDT" className="w-[30rem] h-auto max-w-3xl" />
+                    <div className='w-full flex items-center justify-center mx-4 mt-4 '>
+                        <div className='w-1/2 flex justify-end '>
+                            <img src={cargoWIDT} alt="cargoWIDT" className="w-[30rem] max-w-3xl" />
                         </div>
-                        <div>
-                            <p className='px-16'> &nbsp;&nbsp; &nbsp;&nbsp; CargoWIDT is a warehouse optimization simulator that helps users model and analyze picking operations in a warehouse environment. The application allows users to create a customized warehouse layout by placing items, standby positions, and exit points on a grid. Users can define various product attributes including weight, picking amount, quantity, and price, with the system automatically calculating picking rates based on inventory distributions.</p>
+                        <div className='w-1/2 flex items-start p-16 '>
+                            <p className='text-lg'> &nbsp;&nbsp; &nbsp;&nbsp; <strong>CargoWIDT</strong> is a warehouse optimization simulator that helps users model and analyze picking operations in a warehouse environment. The application allows users to create a customized warehouse layout by placing items, standby positions, and exit points on a grid. Users can define various product attributes including weight, picking amount, quantity, and price, with the system automatically calculating picking rates based on inventory distributions.</p>
                         </div>
                     </div>
                 </div>
